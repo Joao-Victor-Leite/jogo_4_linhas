c577cad9ed97516e00b8a3e2f884a6dec60d6032
João Victor Leite <101682973+Joao-Victor-Leite@users.noreply.github.com>
2024-03-23 16:32:43 -0300

    Correção e documentação das funções

--- client.py
+++ client.py
@@ -1,54 +1,69 @@
 import xmlrpc.client 
 import time
 import sys
+import os
 from port import *
 
-proxy = xmlrpc.client.ServerProxy("http://localhost:8080")
+proxy = xmlrpc.client.ServerProxy("http://localhost:8080", allow_none=True)
 
-
+# Recebe o identificador e simbolo do jogador
 player_id = int(input("Você é o jogador 0 ou 1? "))
 player_symbol = input("Você quer ser o jogador X ou O? ")
 
-validado = proxy.player_validation(player_id, player_symbol)
-if validado:
+# Valida o jogador no servidor
+validated = proxy.player_validation(player_id, player_symbol)
+if validated:
     print("Você foi validado com sucesso. Aguardando outro jogador...")
     time.sleep(10)
 else:
     print("Não foi possível validar. Talvez o símbolo já tenha sido escolhido ou a entrada seja inválida.")
     sys.exit(0)
-""" 
-while TRUE:
-jogador 0 input --> server
-.......
-verifica o turno
-se for entra no loop tentando fazer a jogada
-jogador 1 input --> server
-
-
-faco a jogada
-entra no loop
-so sai quando for a jogada dnv
-
-"""
-
-teste = proxy.draw_board()
-for linha in teste:
-    print(linha)
 
 while True:
-    player_input = int(input(f"Jogador, escolha uma coluna (0-8): "))
-    if proxy.set_player_move(player_input, player_symbol):
-        teste_2 = proxy.draw_board()
-        for linha in teste_2:
+    # Verifica se um jogador já ganhou
+    if proxy.get_game_status():
+        break
+
+    os.system('clear')
+
+    # Verifica se é o turno do jogador atual
+    if proxy.get_turn_check() == player_id:
+
+        # Mostra o tabuleiro
+        current_board = proxy.draw_board()
+        for linha in current_board:
             print(linha)
 
-        if proxy.check_victory(player_symbol):
-            print("Jogador venceu!")
-            break
-    else:
-        print("Coluna cheia, escolha outra.")
+        player_input = int(input(f"Jogador, escolha uma coluna (0-8): "))
 
-"""     if all(board[line][column] != " " for line in range(9) for column in range(9)):
-        print("Empate!")
-        break """
+        # Valida se a jogada foi feita com sucesso
+        if proxy.set_player_move(player_input, player_symbol):
+            
+            # Verifica se o jogador atual ganhou a partida
+            if proxy.check_victory(player_symbol):
+                proxy.set_game_status()
+                break
+
+            # Altera o turno para o próximo jogador
+            proxy.set_turn_check()
+
+            # Mostra o tabuleiro
+            current_board = proxy.draw_board()
+            for linha in current_board:
+                print(linha)
+
+        else:
+            print("Coluna cheia, escolha outra.")
+
+# Mostra o tabuleiro
+current_board = proxy.draw_board()
+for linha in current_board:
+    print(linha)
+
+# Mostra uma mensagem para ambos os jogadores informando o vencedor
+if proxy.get_game_status():
+    player_winner = proxy.get_turn_check()
+    print(f"Jogador {player_winner} venceu!")
+
+
 
--- server.py
+++ server.py
@@ -6,72 +6,147 @@ import sys
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
-players = {'X': None, 'O': None}
-""" a = {symbol: None, winner: False} """
+players = {'X': None, 
+           'O': None}
+
+turn_check = {'player': 0, 
+              'winner': False}
 
 
+def create_board():
+    """
+    Cria e retorna um novo tabuleiro de jogo.
 
-def create_board(): #done
+    Retorna:
+        list: Um tabuleiro 9x9 vazio.
+    """
+
     return [[" " for _ in range(9)] for _ in range(9)]
 
 board = create_board()
 
-def draw_board(): #done
-    # Cria uma lista para armazenar as linhas do tabuleiro, incluindo os separadores
+def draw_board():
+    """
+    Desenha o tabuleiro de jogo atual.
+
+    Retorna:
+        list: Uma lista de strings que representam o tabuleiro atual.
+    """
+
     board_representation = []
 
     for linha in board:
-        # Constrói a representação da linha atual do board e adiciona à representação
         board_representation.append("|" + "|".join([" " + cell + " " for cell in linha]) + "|")
-        
-        # Adiciona o separador de linhas após cada linha do board
     
     board_representation.append("+---" * 9 + "+")
-    # Retorna a representação do tabuleiro como uma lista de strings
+    
     return board_representation
 
-
 def set_player_move(column, player):
-    #Função para registrar o movimento do jogador
+    """
+    Registra o movimento de um jogador no tabuleiro.
+
+    Parâmetros:
+        column (int): O índice da coluna onde o jogador deseja fazer o movimento.
+        player (str): O símbolo do jogador ('X' ou 'O').
+
+    Retorna:
+        bool: True se o movimento foi bem-sucedido, False caso contrário.
+    """
+
     for line in reversed(range(9)):
         if board[line][column] == " ":
             board[line][column] = player
             return True
     return False
 
+def get_turn_check():
+    """
+    Retorna o turno atual do jogo.
 
-def check_victory(player): #done
-    # Verifica linhas, colunas e diagonais para encontrar 4 em linha
-    linhas = len(board)
-    colunas = len(board[0])
+    Retorna:
+        int: O identificador do jogador que deve realizar o próximo movimento (0 ou 1).
+    """
+    return turn_check['player']
+
+def set_turn_check():
+    """
+    Alterna o turno do jogo entre os jogadores.
+    """
+
+    if turn_check['player'] == 0:
+        turn_check['player'] = 1
+    else:
+        turn_check['player'] = 0
+    return
+
+def check_victory(player): 
+    """
+    Verifica se um jogador venceu o jogo seja por linha, coluna ou diagonal.
+
+    Parâmetros:
+        player (str): O símbolo do jogador ('X' ou 'O').
+
+    Retorna:
+        bool: True se o jogador venceu, False caso contrário.
+    """
+
+    lines = len(board)
+    columns = len(board[0])
     
     # Verifica linhas
-    for linha in range(linhas):
-        for coluna in range(colunas - 3):
-            if board[linha][coluna] == player and all(board[linha][coluna + i] == player for i in range(4)):
+    for line in range(lines):
+        for column in range(columns - 3):
+            if board[line][column] == player and all(board[line][column + i] == player for i in range(4)):
                 return True
     
     # Verifica colunas
-    for coluna in range(colunas):
-        for linha in range(linhas - 3):
-            if board[linha][coluna] == player and all(board[linha + i][coluna] == player for i in range(4)):
+    for column in range(columns):
+        for linha in range(lines - 3):
+            if board[line][column] == player and all(board[line + i][column] == player for i in range(4)):
                 return True
     
     # Verifica diagonais (positivas e negativas)
-    for linha in range(linhas - 3):
-        for coluna in range(colunas - 3):
-            if board[linha][coluna] == player and all(board[linha + i][coluna + i] == player for i in range(4)):
+    for line in range(lines - 3):
+        for column in range(columns - 3):
+            if board[line][column] == player and all(board[line + i][column + i] == player for i in range(4)):
                 return True
-            if board[linha + 3][coluna] == player and all(board[linha + 3 - i][coluna + i] == player for i in range(4)):
+            if board[line + 3][column] == player and all(board[line + 3 - i][column + i] == player for i in range(4)):
                 return True
     
     return False
 
+def get_game_status():
+    """
+    Retorna o status atual do jogo.
+
+    Retorna:
+        bool: True se o jogo terminou com um vencedor, False caso contrário.
+    """
+    return turn_check['winner']
+
+def set_game_status():
+    """
+    Define o status do jogo como terminado.
+    """
+
+    turn_check['winner'] = True
+    return True
 
 def player_validation(player_id, player_symbol):
+    """
+    Valida a conexão de um jogador, verificando o símbolo escolhido e se o jogador já está conectado.
+
+    Parâmetros:
+        player_id (str): O identificador do jogador.
+        player_symbol (str): O símbolo escolhido pelo jogador ('X' ou 'O').
+
+    Retorna:
+        bool: True se o jogador for validado com sucesso, False caso contrário.
+    """
     global players
 
-     # Verifica se o símbolo é válido
+    # Verifica se o símbolo é válido
     if player_symbol.upper() not in ['X', 'O']:
         return False
 
@@ -85,18 +160,22 @@ def player_validation(player_id, player_symbol):
     # Verifica se ambos os jogadores estão conectados
     if all(players.values()):
         return True  # Ambos os jogadores estão prontos
-    else:
-        return True  # Jogador validado, mas aguardando o outro jogador
+    
+    return True
+    
 
-
-with SimpleXMLRPCServer((HOST,PORT), requestHandler=RequestHandler) as server:
+with SimpleXMLRPCServer((HOST, PORT), requestHandler=RequestHandler, allow_none=True) as server:
     server.register_introspection_functions()
 
     server.register_function(create_board, "create_board")
     server.register_function(draw_board, "draw_board")
-    server.register_function(player_validation, "player_validation")
     server.register_function(set_player_move, "set_player_move")
+    server.register_function(get_turn_check, "get_turn_check")
+    server.register_function(set_turn_check, "set_turn_check")
     server.register_function(check_victory, "check_victory")
+    server.register_function(get_game_status, "get_game_status")
+    server.register_function(set_game_status, "set_game_status")
+    server.register_function(player_validation, "player_validation")
 
     print(f'Serving XML-RPC on {HOST} port {PORT}')
     try:
